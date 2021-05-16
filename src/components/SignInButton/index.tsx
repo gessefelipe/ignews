2e5696b560ec54014781f5c3b681c4a275842747
Gessé Felipe <gessefelipe.gf@hotmail.com>
2021-05-15 22:34:19 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,30 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import { useState } from 'react';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const [ isUserLoggedIn, setIsUserLoggedIn ] = useState(false);
+  return isUserLoggedIn ? (
+    <button 
+      type="button"
+      onClick={() => setIsUserLoggedIn(false)}
+      className={styles.signInButton}
+    >
+      <FaGithub color="#04D361" />
+      Gessé Felipe
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+      type="button"
+      onClick={() => setIsUserLoggedIn(true)}
+      className={styles.signInButton}
+    >
+      <FaGithub color="#EBA417" />
+      Sign in with Github
+    </button>
+  );
+}
